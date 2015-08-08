1f333fcdc665741f57024cecc2becc62ec50b9a2
Owen Lamb <owen.lamb@gmail.com>
2015-08-08 13:14:26 +0100

    Update bike_spec.rb

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,16 +1,15 @@
 require 'Bike_Class'
 
 describe Bike do
+  it {is_expected.to respond_to(:broken?)}
 
-	it {is_expected.to respond_to(:broken?)}
-
-	it "can be reported broken" do
-		subject.report_broken
-		expect(subject).to be_broken
-	end
-
-	it "can be fixed" do
-		subject.fix
-		expect(subject.working).to be true
-	end
+  it "can be reported broken" do
+    subject.report_broken
+    expect(subject).to be_broken
+  end
+  
+  it "can be fixed" do
+    subject.fix
+    expect(subject.working).to be true
+  end
 end
