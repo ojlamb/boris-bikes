fa0bfa70c7602d1de7af6fa4092d7d090a0be82a
Owen Lamb <owen.lamb@gmail.com>
2015-08-08 13:12:55 +0100

    Update Bike_Class.rb

--- lib/Bike_Class.rb
+++ lib/Bike_Class.rb
@@ -1,23 +1,23 @@
 class Bike
   DEFAULT_WORKING = true
-
   attr_accessor :bike
   attr_accessor :working
 
   def initialize
-		@bike
-	  @working = DEFAULT_WORKING
+    @bike
+    @working = DEFAULT_WORKING
   end
 
   def report_broken
-	  @working = false
+    @working = false
   end
-
+	
   def broken?
-	  @working == false ? true : false
+    @working == false ? true : false
   end
-
+	
   def fix
-	  @working = true
+    @working = true
   end
 end
+
