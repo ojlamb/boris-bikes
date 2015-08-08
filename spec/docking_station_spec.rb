8a054c49f7c044e27fe1bca2d1823d063eeea888
Owen Lamb <owen.lamb@gmail.com>
2015-08-08 13:15:28 +0100

    Update docking_station_spec.rb

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,12 +2,12 @@ require 'DockingStation_Class'
 require 'support/shared_examples_for_bike_container'
 
 describe DockingStation do
-	it_behaves_like BikeContainer
-	let(:bike) {double(:bike, {:broken? => false, :working=>true})}
-	let(:broken_bike) {double(:bike, {:broken? =>true} )}
+  it_behaves_like BikeContainer
+  let(:bike) {double(:bike, {:broken? => false, :working=>true})}
+  let(:broken_bike) {double(:bike, {:broken? =>true} )}
 
-	it "releases working bike" do #releases a bike that is working
-		subject.dock bike
+  it "releases working bike" do #releases a bike that is working
+    subject.dock bike
 		bike = subject.release_bike
 		expect(bike.working).to eq true
 	end
@@ -31,5 +31,4 @@ describe DockingStation do
 			expect{subject.dock bike}.to raise_error 'DockingStation full'
 		end
 	end
-
 end
