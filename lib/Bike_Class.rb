9d524ae69ba5fa7fe920a83a88cabb1c00052a6c
Makers Laptop 22 <makerslaptop22@makerss-mbp-6.home>
2015-08-08 13:10:13 +0100

    edited style

--- lib/Bike_Class.rb
+++ lib/Bike_Class.rb
@@ -1,23 +1,23 @@
 class Bike
-	DEFAULT_WORKING = true
+  DEFAULT_WORKING = true
 
-	attr_accessor :bike
-	attr_accessor :working
+  attr_accessor :bike
+  attr_accessor :working
 
-	def initialize
-		@bike
-		@working = DEFAULT_WORKING
-	end
+  def initialize
+	  @bike
+	  @working = DEFAULT_WORKING
+  end
 
-	def report_broken
-		@working = false
-	end
+  def report_broken
+	  @working = false
+  end
 
-	def broken?
-		@working == false ? true : false
-	end
+  def broken?
+	  @working == false ? true : false
+  end
 
-	def fix
-		@working = true
-	end
+  def fix
+	  @working = true
+  end
 end
